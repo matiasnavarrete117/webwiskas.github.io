3fdfe3c0d2c1bf9e5f0b5d2718cb9de7129e5259
Matías Navarrete <tildo2001@gmail.com>
2024-12-04 23:03:52 -0300

    Commit de respaldo antes de sobrescribir

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  content: [
+    "./*.html", // Incluye todos los archivos HTML en la raíz
+    "./src/**/*.{html,js}" // Incluye subcarpetas si las tienes
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
